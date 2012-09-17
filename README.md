def4805b98103fd93f4d893d8c6389b3f4ecbd9f
zygmuntz <zajac.zygmunt@gmail.com>
2012-09-17 20:05:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 kaggle-bestbuy_small
 ====================
+
+See [http://fastml.com/best-buy-mobile-contest/](http://fastml.com/best-buy-mobile-contest/) for description.
---
+++ train.py
@@ -0,0 +1,80 @@
+import sys, csv, re
+
+def prepare( query ):
+	query = re.sub( r'[\W]', '', query )
+	query = query.lower()
+	return query
+
+popular_skus = [9854804, 2107458, 2541184, 2670133, 2173065]
+
+input_file = sys.argv[1]
+test_file = sys.argv[2]
+output_file = sys.argv[3]
+
+i = open( input_file )
+reader = csv.reader( i )
+
+t = open( test_file )
+
+headers = reader.next()
+mapping = {}
+
+for line in reader:
+	query = line[3]
+	sku = line[1]
+	# print "%s -> %s" % ( query, sku )
+	
+	query = prepare( query )
+	
+	try:
+		mapping[query][sku] += 1
+	except KeyError:
+		try:
+			mapping[query][sku] = 1
+		except KeyError:
+			mapping[query] = {}
+			mapping[query][sku] = 1
+
+		
+#print mapping
+#sys.exit( 0 )
+
+reader = csv.reader( t )
+headers = reader.next()
+
+o = open( output_file, 'wb' )
+writer = csv.writer( o, delimiter = " " )
+
+n = 0
+m = 0
+
+for line in reader:
+	n += 1
+	query = line[2]
+	query = prepare( query )
+	
+	if query in mapping:
+
+		m += 1
+		skus = []
+		
+		for sku in sorted( mapping[query], key=mapping[query].get, reverse = True ):
+			skus.append( sku )
+		#print skus
+
+		'''
+		if len( mapping[query] ) > 1:
+			print "mapping:"
+			print mapping[query]
+			print "skus:"
+			print skus
+		'''
+		
+		skus.extend( popular_skus )
+		skus = skus[0:5]
+	else:
+		skus = popular_skus
+		
+	writer.writerow( skus )
+		
+print "%s / %s (%s)" % ( m, n, 1.0 * m / n )
